b1c44242c3daea44d0b8e50c37ef8a3f1eb80674
hiroshiki <hiroshishikida40@gmail.com>
2024-02-25 18:02:20 +0900

    ToDoリスト一覧API作成

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::apiResource('tasks','App\Http\Controllers\TaskController');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
